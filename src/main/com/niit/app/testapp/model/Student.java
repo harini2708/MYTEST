8cd69df496884643c399d45f8cf1a5c6caef22c5
harini2708 <61176992+harini2708@users.noreply.github.com>
2020-02-22 21:46:31 +0530

    Create Student.java
    new model added

---
+++ src/main/com/niit/app/testapp/model/Student.java
@@ -0,0 +1,38 @@
+package com.niit.app.testapp.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "student")
+public class Student implements Serializable {
+
+	private static final long serialVersionUID = 8436097833452420298L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String studentId;
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+
+}
